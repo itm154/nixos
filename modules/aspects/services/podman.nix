8813b1add9b3837579a56b46496c6a12058ca791
itm154 <ashrulfahmi@gmail.com>
2026-08-02 22:15:32 +0800

    Try to fix podman autostart

--- modules/aspects/services/podman.nix
+++ modules/aspects/services/podman.nix
@@ -23,6 +23,7 @@
           podman = {
             enable = true;
             dockerCompat = true;
+            dockerSocket.enable = true;
             defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
           };
         };
@@ -34,6 +35,7 @@
 
           # Beware that the podman group membership is effectively equivalent
           # to being root, just like with Docker! Consider using rootless podman.
+          # dockerSocket needs user to be in this group to work
           # extraGroups = [
           #   "podman"
           # ];
@@ -45,17 +47,42 @@
           };
         };
 
+        # Needed for nginx
+        boot.kernel.sysctl = {
+          "net.ipv4.ip_unprivileged_port_start" = 80;
+        };
+
+        environment.sessionVariables = {
+          DOCKER_HOST = "unix://$${XDG_RUNTIME_DIR}/podman/podman.socket";
+        };
+
         environment.systemPackages = with pkgs; [
           docker-compose
           podman-compose
         ];
       };
+
     homeManager = { pkgs, ... }: {
       # The default compose providers are docker-compose and podman-compose. If installed, docker-compose takes precedence since it is the original implementation of the Compose specification.
       services.podman.settings.containers = {
-        compose_providers = [ "${pkgs.podman-compose}/bin/podman-compose" ];
-        # or
-        # compose_providers = [ "${pkgs.docker}/bin/docker-compose" ];
+        compose_providers = [
+          "${pkgs.docker-compose}/bin/docker-compose"
+          "${pkgs.podman-compose}/bin/podman-compose"
+        ];
+      };
+
+      systemd.user.services.podman-restart = {
+        Unit = {
+          Description = "Start podman containers on Boot";
+        };
+        Install = {
+          WantedBy = [ "default.target" ];
+        };
+        Service = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+          ExecStart = "${pkgs.podman}/bin/podman restart --all --filter restart-policy=always --filter restart-policy=unless-stopped";
+        };
       };
     };
   };
