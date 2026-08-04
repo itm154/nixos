5e780cddd72128dba87e7aee07a7c22ca46e347c
itm154 <ashrulfahmi@gmail.com>
2026-08-04 15:38:58 +0800

    Add discord/vencord settings

--- modules/aspects/applications/nixcord.nix
+++ modules/aspects/applications/nixcord.nix
@@ -13,6 +13,84 @@
       programs.nixcord = {
         enable = true;
         discord.vencord.enable = true;
+        config = {
+          useQuickCss = true;
+          plugins = {
+            blurNsfw.enable = true;
+            callTimer.enable = true;
+            clearUrls.enable = true;
+            fakeProfileThemes.enable = true;
+            gameActivityToggle.enable = true;
+            hideMedia.enable = true;
+            reverseImageSearch.enable = true;
+            silentTyping.enable = true;
+            validUser.enable = true;
+            vcNarrator = {
+              voice = null;
+            };
+          };
+        };
+
+        extraConfig.plugins = {
+          fakeNitro = {
+            useHyperLinks = true;
+          };
+          messageClickActions = {
+            enableDeleteOnClick = true;
+            enableDoubleClickToEdit = true;
+            enableDoubleClickToReply = true;
+            requireModifier = false;
+          };
+          musicRichPresence = {
+            showLastFmLogo = true;
+          };
+          noBlockedMessages = {
+            applyToIgnoredUsers = true;
+            ignoreBlockedMessages = false;
+            ignoreMessages = false;
+          };
+          platformIndicators = {
+            badges = true;
+          };
+          showHiddenChannels = {
+            hideUnreads = true;
+          };
+          showMeYourName = {
+            displayNames = false;
+            friendNicknames = "dms";
+            inReplies = false;
+            mode = "user-nick";
+          };
+          silentTyping = {
+            contextMenu = true;
+            isEnabled = true;
+            showIcon = false;
+          };
+          translate = {
+            shavian = true;
+            sitelen = true;
+            target = "en";
+            toki = true;
+          };
+        };
+
+        quickCss = ''
+          /**
+           * @name Rounded Discord
+           * @author MarkChan0225
+           * @description A theme which make your Discord becomes rounded
+           * @version 1.1.1
+           * @invite 98gAY7v6y7
+           * @authorId 608821416212692993
+           * @source https://github.com/MarkChan0225/RoundedDiscord
+          */
+
+          @import url(https://MarkChan0225.github.io/RoundedDiscord/src/roundeddiscord.css);
+
+          :root {
+            --radius: 16px;
+          }
+        '';
       };
     };
   };
