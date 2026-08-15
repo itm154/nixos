ba6ec94a023a1141c04fda150fe5b3f980bfd9ff
itm154 <ashrulfahmi@gmail.com>
2026-08-15 13:27:31 +0800

    Add yazi plugins
    - Recycle Bin
    - Media info
    - Archive tools

--- modules/aspects/shell/tools/yazi.nix
+++ modules/aspects/shell/tools/yazi.nix
@@ -10,17 +10,109 @@
             sort_by = "natural";
             sort_dir_first = true;
           };
+          opener = {
+            extract = [
+              {
+                run = "${pkgs.ouch}/bin/ouch d -y \"$@\"";
+                desc = "Extract here with ouch";
+                for = "unix";
+              }
+            ];
+          };
+          plugin = {
+            prepend_preloaders = [
+              # Mediainfo
+              {
+                mime = "{audio,video,image}/*";
+                run = "mediainfo";
+              }
+              {
+                mime = "application/{subrip,postscript,illustrator,dvb.ait,vnd.adobe.illustrator,eps}";
+                run = "mediainfo";
+              }
+              {
+                url = "*.{ai,eps,ait}";
+                run = "mediainfo";
+              }
+              {
+                mime = "{image}/*";
+                run = "mediainfo --no-metadata";
+              }
+              {
+                mime = "{video}/*";
+                run = "mediainfo --no-preview";
+              }
+            ];
+            prepend_previewers = [
+              # Mediainfo
+              {
+                mime = "{audio,video,image}/*";
+                run = "mediainfo";
+              }
+              {
+                mime = "application/{subrip,postscript,illustrator,dvb.ait,vnd.adobe.illustrator,eps}";
+                run = "mediainfo";
+              }
+              {
+                url = "*.{ai,eps,ait}";
+                run = "mediainfo";
+              }
+              {
+                mime = "{image}/*";
+                run = "mediainfo --no-metadata";
+              }
+              {
+                mime = "{video}/*";
+                run = "mediainfo --no-preview";
+              }
+
+              # Ouch
+              {
+                mime = "application/{*zip,tar,bzip2,7z*,rar,xz,zstd,java-archive}";
+                run = "ouch";
+              }
+            ];
+          };
+
+          tasks = {
+            image_alloc = 1073741824;
+          };
         };
-        # TODO
-        # plugins = with pkgs; {
-        #   mediainfo = yaziPlugins.mediainfo;
-        #   ouch = yaziPlugins.ouch;
-        #   duckdb = {
-        #     setup = true;
-        #     package = yaziPlugins.duckdb;
-        #   };
-        # };
+        plugins = with pkgs.yaziPlugins; {
+          mediainfo = mediainfo;
+          ouch = ouch;
+          recycle-bin = {
+            package = recycle-bin;
+            setup = true;
+          };
+        };
+        keymap = {
+          mgr.prepend_keymap = [
+            # Recycle bin
+            {
+              on = [
+                "<C-b>"
+                "b"
+              ];
+              run = "plugin recycle-bin";
+              desc = "Open Recycle Bin menu";
+            }
+
+            # Ouch
+            {
+              on = [ "C" ];
+              run = "plugin ouch";
+              desc = "Compress with ouch";
+            }
+          ];
+        };
       };
+
+      # Plugin dependencies
+      home.packages = with pkgs; [
+        mediainfo
+        imagemagick
+      ];
 
       xdg.configFile."yazi/theme.toml".source = "${
         pkgs.fetchFromGitHub {
